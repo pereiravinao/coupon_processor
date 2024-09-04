8a48e6261706940b5eb987bbad931491fcafd96e
Vinicius Norberto <vpereira.norberto@gmail.com>
2024-09-03 21:48:49 -0300

    Inicializa Projeto

---
+++ src/main/java/com/pereiravinao/coupon_processor/CouponProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.pereiravinao.coupon_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pereiravinao/coupon_processor/CouponProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pereiravinao.coupon_processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
